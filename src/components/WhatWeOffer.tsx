1cc892b6e93eebcdfa539b7410558fc17422f7cb
Sonawane-Shubham <sssonawane047@gmail.com>
2025-08-01 20:29:00 +0530

    make what we offer section view all button function and added icons to service card

--- src/components/WhatWeOffer.tsx
+++ src/components/WhatWeOffer.tsx
@@ -1,6 +1,18 @@
-import React from "react";
-import OfferItem from "./OfferItem";
+"use client";
+import React, { useState } from "react";
+import { ourServices } from "@/data/ourServices";
+import ServiceCard from "./ServiceCard";
 const WhatWeOffer = () => {
+  const INITIAL_VISIBLE_SERVICE_COUNT = 3;
+  const [viewAll, setViewAll] = useState<boolean>(false);
+  const visibleServices = viewAll
+    ? ourServices
+    : ourServices.slice(0, INITIAL_VISIBLE_SERVICE_COUNT);
+
+  const handleToggleViewAll = () => {
+    setViewAll(!viewAll);
+  };
+
   return (
     <section className="w-full ">
       <div className="w-full lg:max-w-5/6 mx-auto px-4 sm:px-6 lg:px-0 py-12 sm:py-16 lg:py-20">
@@ -8,14 +20,17 @@ const WhatWeOffer = () => {
           <h2 className="text-2xl sm:text-3xl lg:text-4xl font-bold text-text-primary">
             What We Offer
           </h2>
-          <button className="bg-primary-muted hover:bg-primary text-base lg:text-lg font-semibold px-6 py-3 text-center rounded-md cursor-pointer transition-all duration-300 ">
-            View All
+          <button
+            className="bg-primary-muted hover:bg-primary text-base lg:text-lg font-semibold px-6 py-3 text-center rounded-md cursor-pointer transition-all duration-300 "
+            onClick={handleToggleViewAll}
+          >
+            {viewAll ? "View Less" : "View All"}
           </button>
         </div>
         <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          <OfferItem />
-          <OfferItem />
-          <OfferItem />
+          {visibleServices.map((service) => (
+            <ServiceCard key={service.id} service={service} />
+          ))}
         </div>
       </div>
     </section>
---
+++ src/data/ourServices.tsx
@@ -0,0 +1,40 @@
+"user client";
+import {
+  AccessControllAndIntercoms,
+  SecurityCameras,
+  NewtworkingAndWiFi,
+} from "@/icons";
+export const ourServices = [
+  {
+    id: "service-101",
+    icon: <AccessControllAndIntercoms />,
+    service: "Access Control & Intercoms",
+    description:
+      "Wired or wireless intercom systems with secure app-based access.",
+  },
+  {
+    id: "service-102",
+    icon: <SecurityCameras />,
+    service: "Security Cameras & Alarms",
+    description: "24/7 surveillance, motion sensors, and real-time alerts.",
+  },
+  {
+    id: "service-103",
+    icon: <NewtworkingAndWiFi />,
+    service: "Networking & Wi-Fi Systems",
+    description: "Commercial-grade Wi-Fi, VPNs, and structured wiring.",
+  },
+  {
+    id: "service-104",
+    icon: <AccessControllAndIntercoms />,
+    service: "Smart Lighting & Thermostats",
+    description:
+      "Wired or wireless intercom systems with secure app-based access.",
+  },
+  {
+    id: "service-101",
+    icon: <SecurityCameras />,
+    service: " Home Theater & AV Systems",
+    description: "24/7 surveillance, motion sensors, and real-time alerts.",
+  },
+];
